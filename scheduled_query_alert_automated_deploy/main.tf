569fcad2dad3445ba59290db3362a5b653a660fe
Nicolas Mays <nicolasmays@microsoft.com>
2021-03-23 19:23:13 -0400

    dropped var prefix.

--- scheduled_query_alert_automated_deploy/main.tf
+++ scheduled_query_alert_automated_deploy/main.tf
@@ -9,10 +9,10 @@ terraform {
 }
 
 provider "azurerm" {
-    subscription_id = var.AZ_SUBSCRIPTION_ID
-    client_id       = var.AZ_CLIENT_ID
-    client_secret   = var.AZ_CLIENT_SECRET
-    tenant_id       = var.AZ_TENANT_ID
+    #subscription_id = var.AZ_SUBSCRIPTION_ID
+    #client_id       = var.AZ_CLIENT_ID
+    #client_secret   = var.AZ_CLIENT_SECRET
+    #tenant_id       = var.AZ_TENANT_ID
     features {}
 }
 
